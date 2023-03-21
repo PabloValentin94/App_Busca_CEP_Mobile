f2c8b4b682d1b74ecddb4c93cd71d1e69a407b23
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2023-03-21 15:58:37 -0300

    Criando a tela de busca de bairros através de um estado e uma cidade.

--- App_Busca_CEP/App_Busca_CEP/View/Bairros_por_Cidade.xaml.cs
+++ App_Busca_CEP/App_Busca_CEP/View/Bairros_por_Cidade.xaml.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using System.Collections.ObjectModel;
+
+using App_Busca_CEP.Model;
+using App_Busca_CEP.Service;
+using Xamarin.Forms.Internals;
+
 namespace App_Busca_CEP.View
 {
 
@@ -16,16 +22,59 @@ namespace App_Busca_CEP.View
     public partial class Bairros_por_Cidade : ContentPage
     {
 
+        string[] estados = {"AC", "AL", "AP", "AM", "BA", "CE", "ES", "GO", "MA", 
+                            "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", 
+                            "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO", "DF"};
+
+        ObservableCollection<Cidade> lista_cidades = new ObservableCollection<Cidade>();
+
+        ObservableCollection<Bairro> lista_bairros = new ObservableCollection<Bairro>();
 
         public Bairros_por_Cidade()
         {
 
             InitializeComponent();
 
+            pck_estado.ItemsSource = estados;
+            pck_cidade.ItemsSource = lista_cidades;
+            //lst
+
         }
 
         private void btn_busca_Clicked(object sender, EventArgs e)
         {
+
+
+
+        }
+
+        private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            Picker picker_estado = sender as Picker;
+
+            string estado = picker_estado.SelectedItem as string;
+
+            List<Cidade> listagem_cidades = await Data_Service.GetCidadesByUF(estado);
+
+            lista_cidades.Clear();
+
+            listagem_cidades.ForEach(i => lista_cidades.Add(i));
+
+        }
+
+        private async void pck_cidade_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            /*Picker picker_cidade = sender as Picker;
+
+            Cidade cidade_selecionada = picker_cidade.SelectedItem as Cidade;
+
+            List<Bairro> lista_bairros = await Data_Service.GetBairrosByIDCidade(cidade_selecionada.id);
+
+            lista_cidades.Clear();
+
+            lista_bairros.ForEach(i => lista_bairros.Add(i));*/
 
         }
 
